8a53852ad98d7f53f9bfced8d0700a80b623ef9c
Marcus Krahl <mail@marcuskrahl.de>
2016-04-06 09:50:05 +0200

    automatically detect number of cores for CPU usage in xmobar

--- xmobar.hs
+++ xmobar.hs
@@ -12,7 +12,7 @@ Config {
     lowerOnStart = True,
     commands = [
         Run Weather "EDDC" ["-t","<tempC>C <skyCondition>","-L","64","-H","77","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 9000,
-        Run MultiCpu ["-t","Cpu: <total0>% <total1>% <total2>% <total3>% <total4>% <total5>% <total6>% <total7>% ","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
+        Run MultiCpu ["-t","Cpu: <autototal>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3","-S","True"] 10,
         Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         Run DynNetwork ["-t","Net: <rx>, <tx>","-S","True","-d","1","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC", "-w","9"] 10,
         Run Wireless "wlp4s0" ["-t","Wifi: <essid> <qualityvbar>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 100,
